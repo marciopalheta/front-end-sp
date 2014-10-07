4dcf4f3e5f778397c06234214f29ffd2e98577c2
Marcio Palheta <web4634@caelum-Satellite-L655>
2014-10-07 17:15:47 -0300

    fim da aula 4

---
+++ mirrorfashion/js/home.js
@@ -0,0 +1,36 @@
+var form = document.querySelector("#form-busca");
+var input = document.querySelector("#q");
+var banner = document.querySelector(".destaque img");
+var imagens = ["img/destaque-home.png", "img/destaque-home-2.png"];
+var banerAtual = 0;
+
+function trocaBanner(){
+	banerAtual = (banerAtual+1) % imagens.length;
+	banner.src = imagens[banerAtual];
+}
+
+var timer = setInterval(trocaBanner, 4000);
+var controle = document.querySelector(".pause");
+
+form.onsubmit = function(){
+	if(input.value == ''){
+		alert("Informe o valor do campo");
+		input.style.backgroundColor="red";
+		return false;
+	}
+};
+
+input.onfocus = function(){
+	input.style.backgroundColor="white";
+};
+
+controle.onclick = function(){
+	if(controle.className == "pause"){
+		clearInterval(timer);
+		controle.className = "play";
+	}else{
+		timer = setInterval(trocaBanner, 4000);
+		controle.className = "pause";
+	}
+	return false;
+};
