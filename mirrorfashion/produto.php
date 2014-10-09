2d034d424e9b40d8b823cdbeee413a2ffef02672
Marcio Palheta <web4634@caelum-Satellite-L655>
2014-10-09 14:33:34 -0300

    fim da aula 7

--- mirrorfashion/produto.php
+++ mirrorfashion/produto.php
@@ -1,33 +1,75 @@
 <?php 
 	$cabecalho_title = "Produto MF";
+	$cabecalho_css = '<link rel="stylesheet" href="css/produto.css"';
 	include("cabecalho.php");
 ?>
 
-<div class="produto">
-	<h1>Fuzzy Cardigan</h1>
-	<p>por apenas R$ 129,00</p>
-	<form>
-		<fieldset class="cores">
-			<legend>Escolha a cor:</legend>
+<div class="produto-back">
+	<div class="container">
+		<div class="produto">
+			<h1>Fuzzy Cardigan</h1>
+			<p>por apenas R$ 129,00</p>
+			<form>
+				<fieldset class="cores">
+					<legend>Escolha a cor:</legend>
 			
-			<input type="radio" name="cor" value="verde" id="verde" checked>
-			<label for="verde">
-				<img src="img/produtos/foto2-verde.png" alt="verde">
-			</label>
+					<input type="radio" name="cor" value="verde" id="verde" checked>
+					<label for="verde">
+						<img src="img/produtos/foto2-verde.png" alt="verde">
+					</label>
 			
-			<input type="radio" name="cor" value="rosa" id="rosa">
-			<label for="rosa">
-				<img src="img/produtos/foto2-rosa.png" alt="rosa">
-			</label>
+					<input type="radio" name="cor" value="rosa" id="rosa">
+					<label for="rosa">
+						<img src="img/produtos/foto2-rosa.png" alt="rosa">
+					</label>
 			
-			<input type="radio" name="cor" value="azul" id="azul">
-			<label for="azul">
-				<img src="img/produtos/foto2-azul.png" alt="azul">
-			</label>
-		</fieldset>
+					<input type="radio" name="cor" value="azul" id="azul">
+					<label for="azul">
+						<img src="img/produtos/foto2-azul.png" alt="azul">
+					</label>
+				</fieldset>
+			
+				<fieldset>
+					<legend>Escolha o tamanho</legend>
+					<input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
+				</fieldset>
 		
-		<input type="submit" class="comprar" value="comprar">
-	</form>
+				<input type="submit" class="comprar" value="comprar">
+			</form>
+		</div>
+	
+		<div class="detalhes">
+			<h2>Detalhes do produto</h2>
+			<p>Esse é o melhor casaco de Cardigã que você já viu. Excelente material italiano com estampa desenhada pelos artesãos da comunidade de Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa entrega ajato.</p>
+		
+			<table>
+				<thead>
+					<tr>
+						<th>Característica</th>
+						<th>Detalhes</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Modelo</td>
+						<td>Cardigã 7845</td>
+					</tr>
+					<tr>
+						<td>Material</td>
+						<td>Algodão e poliester</td>
+					</tr>
+					<tr>
+						<td>Cores</td>
+						<td>Azul, rosa e verde</td>
+					</tr>
+					<tr>
+						<td>Lavagem</td>
+						<td>Lvar a mão</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
 
 <?php include("rodape.php");?>
